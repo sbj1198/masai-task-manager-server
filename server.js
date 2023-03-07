b83f21b642c90de00333fd36233db9028f75e388
setu bhaskar jha <sbhaskar1998@gmail.com>
2023-03-07 21:39:56 +0530

    added: new server configs

--- server.js
+++ server.js
@@ -1,10 +1,29 @@
-const jsonServer = require("json-server"); // importing json-server library
-const server = jsonServer.create();
-const router = jsonServer.router("db.json");
-const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 8080; // you can use any port number here; i chose to use 8080
+// const jsonServer = require("json-server"); // importing json-server library
+// const server = jsonServer.create();
+// const router = jsonServer.router("db.json");
+// const middlewares = jsonServer.defaults();
+// const port = process.env.PORT || 8080; // you can use any port number here; i chose to use 8080
 
+// server.use(middlewares);
+// server.use(router);
+
+// server.listen(port);
+
+const jsonServer = require("json-server");
+const cors = require("cors");
+const path = require("path");
+
+const server = jsonServer.create();
+const router = jsonServer.router(path.join(__dirname, "db.json"));
+const middlewares = jsonServer.defaults();
+
+server.use(cors());
+server.use(jsonServer.bodyParser);
 server.use(middlewares);
 server.use(router);
 
-server.listen(port);
+const PORT = 8000;
+
+server.listen(PORT, () => {
+  console.log(`JSON Server is running on http://localhost:${PORT}`);
+});
