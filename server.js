ecc0cced5e6ba3bc96fbef71932ab0320e1c6425
setu bhaskar jha <sbhaskar1998@gmail.com>
2023-03-07 15:13:15 +0530

    added: minor bug

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const jsonserver = require("json-server");
-const server = jsonserver.create();
-const router = jsonserver.router("db.json");
-const middlewares = jsonserver.defaults();
-const port = process.env.PORT || 8080;
+const jsonServer = require("json-server"); // importing json-server library
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 8080; // you can use any port number here; i chose to use 8080
 
 server.use(middlewares);
 server.use(router);
